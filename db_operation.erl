05a0f243fbc1587a50ad5a7c57bec907e888f176
Gonzalo L Petraglia <gonzalpetraglia@gmail.com>
2018-06-19 23:27:37 -0300

    Fix parameters orders

--- db_operation.erl
+++ db_operation.erl
@@ -1,23 +1,18 @@
 -module(db_operation).
 -export([add/2, select/2]).
 
-add(Db, NewRow) ->
-	case NewRow of
-		{N, A, P} -> ok;
-		_ -> throw("Not a triple")
-	case member(Db, NewRow) of
-		false -> [NewRow | Db];
+add(Db, {N, A, P}) ->
+	case lists:member({N, A, P}, Db) of
+		false -> [{N, A, P} | Db];
 		true -> throw("Duplicated entry")
 	end.
 
 filter_db(Db, {address, A}) ->
-	filter(Db, fun({_, Address, _}) -> A == Address end));
+	lists:filter(fun({_, Address, _}) -> A == Address end, Db);
 filter_db(Db, {name, N}) ->
-	filter(Db, fun({Name, _, _}) -> N == Name end));
-filter_db(Db, {phone, Phone}) ->
-	filter(Db, fun({_, _, Phone}) -> P == Phone end));
-filter_db(_, F) ->
-	throw("Invalid field").
+	lists:filter(fun({Name, _, _}) -> N == Name end, Db);
+filter_db(Db, {phone, P}) ->
+	lists:filter(Db, fun({_, _, Phone}) -> P == Phone end, Db).
 
 select(Db, []) ->
 	Db;
